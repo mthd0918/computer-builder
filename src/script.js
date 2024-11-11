0e61c08b2a392d9d8729f6e77dad3a7521c94a15
Hidechika Matayoshi <hmtys0918@icloud.com>
2024-11-11 19:07:51 +0900

    finish

--- src/script.js
+++ src/script.js
@@ -1,6 +1,4 @@
-    // ここからJavaScriptを記述してください。
-    // afterProcess関数
-fetch ('https://api.recursionist.io/random-integer')
+fetch('https://api.recursionist.io/random-integer')
     .then(response => response.json())
     .then(function(data) {
         console.log(data);
